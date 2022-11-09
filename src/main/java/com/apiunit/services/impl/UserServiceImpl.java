43bece557cc5970d39a246cacdcaef28107f61fd
Wkerlyson Batista <wkerlyson.batista@gmail.com>
2022-11-09 17:37:05 -0300

    exception handling for object not found

--- src/main/java/com/apiunit/services/impl/UserServiceImpl.java
+++ src/main/java/com/apiunit/services/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.apiunit.services.impl;
 import com.apiunit.domain.User;
 import com.apiunit.repositories.UserRepository;
 import com.apiunit.services.UserService;
+import com.apiunit.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,6 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User findById(Integer id) {
-        return repository.findById(id).orElse(null);
+        return repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found"));
     }
 }
