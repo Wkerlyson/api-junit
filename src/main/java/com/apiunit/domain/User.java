47bc2dd8245e59e754c6658e5e0dd320d829b26f
Wkerlyson Batista <wkerlyson.batista@gmail.com>
2022-11-09 15:34:18 -0300

    class user resource findById

--- src/main/java/com/apiunit/domain/User.java
+++ src/main/java/com/apiunit/domain/User.java
@@ -4,20 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private Integer id;
     private String name;
-
-    @Column(unique = true)
     private String email;
     private String password;
 }
